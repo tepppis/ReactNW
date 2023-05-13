5786af67ccf82f9dc5faa108c43effc33b9a4afd
Teemu Lackman <teemu.lackman@student.careeria.fi>
2023-05-13 13:49:55 +0300

    Kotitehtävien ratkaisumallit

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -8,6 +8,7 @@ const Posts = () => {
 
 // Komponentin tilan määritys
 const [posts, setPosts] = useState([])
+const [showPosts, setShowPosts] = useState(false)
 
 useEffect(() => {
   fetch("https://jsonplaceholder.typicode.com/posts")
@@ -20,18 +21,20 @@ useEffect(() => {
 
   return (
     <>
-        <h2>Posts from JsonPlaceholder.Typicode</h2>
+        <h2 onClick={() => setShowPosts(!showPosts)}>Posts from JsonPlaceholder.Typicode</h2>
 
         {
           //loopataan läpi taulukollinen olioita  posts &&  koska fetchaus kestää hetken pidempään kuin sivun latautuminen ja voi kaataa ohjelman
-          posts && posts.map(p => 
+          showPosts && posts && posts.map(p => 
 
           <div className='posts' key={p.id}>
 
             {/* kotitehtävä muodostaa mukavampi näkymä ja samalainen piilotustoiminto kuin laskurissa*/}
           {/* <p>{p.userId}</p> */}
 
-          <p>{p.title}</p>
+          <h4>{p.title}</h4>
+          <h5>User ID: {p.userId}</h5>
+          <p>{p.body}</p>
 
           </div>
           ) 
